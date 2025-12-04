1cc9bbef524c48a646afb94b473ea3ff2ef628f1
rlagudwls5518 <rlagudwls5518@naver.com>
2025-12-04 18:16:32 +0900

    refactor: 서비스 계층  메서드 분리

--- src/main/java/baseball/model/GameService.java
+++ src/main/java/baseball/model/GameService.java
@@ -8,14 +8,18 @@ public class GameService {
     public BaseballScore playOneGame(String inputNumber, List<Integer> computerNumbers) {
         NumberManagement numberManagement = new NumberManagement(inputNumber);
         List<Integer> humanNumbers = numberManagement.parsingNumber();
-        BaseballScore baseballScore = new BaseballScore();
-        CalculatorNumber calculatorNumber = new CalculatorNumber(humanNumbers, computerNumbers, baseballScore);
-        return calculatorNumber.calculator();
+
+        return calculatorService(humanNumbers,computerNumbers).calculator();
     }
 
     public List<Integer> generaterNewNumber() {
         RandomNumber randomNumber = new RandomNumber();
         return randomNumber.RandomNumber();
+    }
+
+    private CalculatorNumber calculatorService(List<Integer> humanNumbers, List<Integer> computerNumbers){
+        BaseballScore baseballScore = new BaseballScore();
+        return new CalculatorNumber(humanNumbers, computerNumbers, baseballScore);
     }
 
 }
