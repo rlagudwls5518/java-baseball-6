cf835fbdb6f8212197050d7249d7440e95577a7d
rlagudwls5518 <rlagudwls5518@naver.com>
2025-12-04 17:52:33 +0900

    refactor: 시작 의존성 주입 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,17 @@
 package baseball;
 
 
+import baseball.controller.GameController;
+import baseball.model.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        Input input = new Input();
-
-        GameController gameController = new GameController(input);
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+        GameService gameService = new GameService();
+        GameController gameController = new GameController(input, output, gameService);
         gameController.run();
     }
 }
