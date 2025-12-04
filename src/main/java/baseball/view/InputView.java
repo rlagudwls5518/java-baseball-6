ab06336e3c0eb1e4ba4e03cc0137a2f580c3b4bb
rlagudwls5518 <rlagudwls5518@naver.com>
2025-12-04 18:05:45 +0900

    refactor: 게임 종료 재판단 로직 수정2

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,13 +3,23 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-
+    private final String INPUT_NUMBER_MESSAGE = "숫자를 입력해줏세요 : ";
+    private final int RESTART_GAME_NUMBER = 1;
     public String gameInput(){
-        System.out.println("숫자를 입력해줏세요 : ");
+        System.out.println(INPUT_NUMBER_MESSAGE);
         return readLine();
     }
 
     public String reGameInput(){
         return readLine();
+    }
+
+    public boolean restartGame(String chooseNumber){
+        if (chooseNumber.equals(RESTART_GAME_NUMBER)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
