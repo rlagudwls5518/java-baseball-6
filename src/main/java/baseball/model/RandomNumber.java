e472eb7b64943cd87ecd1f0265f9e51ecfaa9860
rlagudwls5518 <rlagudwls5518@naver.com>
2025-12-04 18:08:26 +0900

    refactor: 랜덤 숫자클래스 상수화

--- src/main/java/baseball/model/RandomNumber.java
+++ src/main/java/baseball/model/RandomNumber.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumber {
+    private final int RANDOM_NUMBER_SIZE = 3;
+    private final int START_NUMBER = 1;
+    private final int END_NUMBER = 9;
 
     public List<Integer> RandomNumber(){
         List<Integer> computer = new ArrayList<>();
 
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < RANDOM_NUMBER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
